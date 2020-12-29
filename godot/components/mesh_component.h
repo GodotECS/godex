a978c4ee66b4d4405b7afec596575d7a8e5387ce
AndreaCatania <info@andreacatania.com>
2020-12-29 09:45:09 +0100

    Added Mesh property to MeshComponent

--- godot/components/mesh_component.h
+++ godot/components/mesh_component.h
@@ -4,15 +4,21 @@
 #define MESH_COMPONENT_H
 
 #include "../../components/component.h"
+#include "scene/resources/mesh.h"
 
 class MeshComponent : public godex::Component {
 	COMPONENT(MeshComponent, DenseVector)
+
+	Ref<Mesh> mesh;
 
 public:
 	MeshComponent();
 
 protected:
-	static void _bind_params();
+	static void _bind_properties();
+
+	void set_mesh(const Ref<Mesh> &p_mesh);
+	Ref<Mesh> get_mesh() const;
 };
 
 #endif
