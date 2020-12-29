b11f6b25ace6d0dc736be2787ca31e48f38e7f0e
AndreaCatania <info@andreacatania.com>
2020-12-29 09:45:09 +0100

    Added Mesh property to MeshComponent

--- godot/components/mesh_component.cpp
+++ godot/components/mesh_component.cpp
@@ -4,4 +4,14 @@
 
 MeshComponent::MeshComponent() {}
 
-void MeshComponent::_bind_params() {}
+void MeshComponent::_bind_properties() {
+	add_property(PropertyInfo(Variant::OBJECT, "mesh", PROPERTY_HINT_RESOURCE_TYPE, "Mesh"), &MeshComponent::set_mesh, &MeshComponent::get_mesh);
+}
+
+void MeshComponent::set_mesh(const Ref<Mesh> &p_mesh) {
+	mesh = p_mesh;
+}
+
+Ref<Mesh> MeshComponent::get_mesh() const {
+	return mesh;
+}
