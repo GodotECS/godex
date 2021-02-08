be7fb270a4620b0d6c6a07e81f2caff60ecc55a0
carlos-ramos-73 <67236141+carlos-ramos-73@users.noreply.github.com>
2021-02-08 01:18:18 -0700

    External documentation can now be made, Syncs with repo's Wiki (#75)
    * Added documentation files.
    * CI - Build Wiki Documentation

--- config.py
+++ config.py
@@ -17,3 +17,20 @@ def has_custom_physics_iterator():
 def has_custom_audio_iterator():
     # TODO enable custom iterator once the audio process system is integrated
     return False
+
+def get_doc_path():
+    return "doc_classes"
+
+
+def get_doc_classes():
+    return [
+        "Component",
+        "DynamicQuery",
+        "ECS",
+        # Disabled until only 'Entity' exists. 'doctool' will generate in 'godot/docs/classes' instead.
+        #"Entity2D",
+        #"Entity3D",
+        "PipelineECS",
+        "System",
+        "WorldECS",
+    ]
