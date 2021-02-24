e730d59f9363539788467f48b757e09dca08ecda
AndreaCatania <info@andreacatania.com>
2021-02-24 12:21:43 +0100

    Added SteadyStorage. Put DenseVectorStorage in its own file
    The SteadyStorage is a lot useful when it's necessary to store big
    components: in those cases, you don't want to reallocate the memory each time a new
    entity is added or removed.
    
    This storage is also useful when you are dealing with libraries like
    the physics engine, audio engine, etc... .
    With those libraries, it's usually necessary to create objects, so thanks
    to this storage it's possible to create components that are fixed in memory,
    so pass the pointer or internal component pointers is safe.
    
    Note, the allocated memory is contiguous.

--- storage/dense_vector.h
+++ storage/dense_vector.h
@@ -1,7 +1,5 @@
 /* Author: AndreaCatania */
-
-#ifndef DENSE_VECTOR_H
-#define DENSE_VECTOR_H
+#pragma once
 
 #include "../ecs.h"
 #include "core/templates/local_vector.h"
@@ -92,76 +90,3 @@ protected:
 		entity_to_data[p_entity] = p_index;
 	}
 };
-
-/// Dense vector storage.
-/// Has a redirection 2-way table between entities and
-/// components (vice versa), allowing to leave no gaps within the data.
-/// The the entity indices are stored sparsely.
-template <class T>
-class DenseVectorStorage : public Storage<T> {
-protected:
-	DenseVector<T> storage;
-
-public:
-	virtual String get_type_name() const override {
-		return "DenseVector[" + String(typeid(T).name()) + "]";
-	}
-
-	virtual void insert(EntityID p_entity, const T &p_data) override {
-		storage.insert(p_entity, p_data);
-		StorageBase::notify_changed(p_entity);
-	}
-
-	virtual bool has(EntityID p_entity) const override {
-		return storage.has(p_entity);
-	}
-
-	virtual Batch<const std::remove_const_t<T>> get(EntityID p_entity, Space p_mode = Space::LOCAL) const override {
-		return &storage.get(p_entity);
-	}
-
-	virtual Batch<std::remove_const_t<T>> get(EntityID p_entity, Space p_mode = Space::LOCAL) override {
-		StorageBase::notify_changed(p_entity);
-		return &storage.get(p_entity);
-	}
-
-	virtual void remove(EntityID p_entity) override {
-		storage.remove(p_entity);
-		// Make sure to remove as changed.
-		StorageBase::notify_updated(p_entity);
-	}
-
-	virtual void clear() override {
-		storage.clear();
-		StorageBase::flush_changed();
-	}
-
-	virtual EntitiesBuffer get_stored_entities() const {
-		return { storage.get_entities().size(), storage.get_entities().ptr() };
-	}
-};
-
-template <class T>
-class DynamicDenseVectorStorage : public DenseVectorStorage<T> {
-	DynamicComponentInfo *dynamic_componente_info = nullptr;
-
-public:
-	DynamicDenseVectorStorage(DynamicComponentInfo *p_info) :
-			dynamic_componente_info(p_info) {}
-
-	virtual void insert_dynamic(EntityID p_entity, const Dictionary &p_data) override {
-		T insert_data;
-
-		// Initialize with defaults.
-		insert_data.__initialize(dynamic_componente_info);
-
-		// Set the custom data if any
-		for (const Variant *key = p_data.next(); key; key = p_data.next(key)) {
-			T::set_by_name(&insert_data, StringName(*key), *p_data.getptr(*key));
-		}
-
-		this->insert(p_entity, insert_data);
-	}
-};
-
-#endif
