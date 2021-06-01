0232069f4d2b43a9c5cae424b41b23e60bbd79a1
AndreaCatania <info@andreacatania.com>
2021-06-01 11:51:41 +0200

    Prepare the test for the PipelineBuilder.

--- tests/test_ecs_pipeline_builder.h
+++ tests/test_ecs_pipeline_builder.h
@@ -3,31 +3,67 @@
 
 #include "tests/test_macros.h"
 
+#include "../components/component.h"
+#include "../databags/databag.h"
 #include "../modules/godot/nodes/ecs_utilities.h"
 #include "../pipeline/pipeline.h"
 #include "../pipeline/pipeline_builder.h"
+#include "../storage/dense_vector_storage.h"
 #include "test_utilities.h"
+
+// The idea to verify that the pipeline is correctly constructed we need to
+// verify the following:
+// 1. The Systems with implicit dependencies are correctly taken into account.
+// 		- The Systems implicit priority is used in this case, where the
+//		  priority is given by the registration order.
+// 		- The Systems implicit dependency, where two system fetch the same
+//		  Compoenents and DataBags mutable, are resolved.
+// 		- The systems that fetch the sane Components and DataBag immutable
+//		  can run in parallel.
+// 2. The Systems with explicit dependencies are correctly taken into account.
+// 		- The Systems explicit priority, build with (`after` and `before`) is
+//		  correctly resolved.
+// 3. The Systems phase is correctly used.
+// 4. The SystemBundles are correctly expanded.
+// 5. The SystemBundles explicit dependencies are take into account.
+// 6. The Query filer `Not` can be used to specialize the fetched component
+//	  so two systems that fetch the same component mutable may run in parallel
+//	  thanks to the specialization given by `Not`.
+// 7. All the above must be valid for C++ and Scripted systems.
+// TODO 8. Test advice mechanism
+
+struct PbComponentA {
+	COMPONENT(PbComponentA, DenseVectorStorage)
+};
+
+struct PbComponentB {
+	COMPONENT(PbComponentB, DenseVectorStorage)
+};
+
+struct PbDatabagA : public godex::Databag {
+	DATABAG(PbDatabagA)
+};
 
 namespace godex_tests {
 
-TEST_CASE("[Modules][ECS] Test Pipeline builder.") {
+TEST_CASE("[Modules][ECS] Initialize PipelineBuilder tests.") {
+	// Just initializes the common Components and Databags used by the following tests.
+	ECS::register_component<PbComponentA>();
+	ECS::register_component<PbComponentB>();
+	ECS::register_databag<PbDatabagA>();
+}
+} // namespace godex_tests
+
+void test_A_system_1(Query<PbComponentA, const PbComponentB> &p_query) {}
+void test_A_system_3(Query<const PbComponentA, const PbComponentB> &p_query, PbDatabagA *p_db_a) {}
+void test_A_system_5(Query<const PbComponentA, const PbComponentB> &p_query, const PbDatabagA *p_db_a) {}
+
+namespace godex_tests {
+
+TEST_CASE("[Modules][ECS] Verify the PipelineBuilder takes into account implicit dependencies and implicit order.") {
 	initialize_script_ecs();
 
-	// The idea to verify that the pipeline is correctly constructed we need to
-	// verify the following:
-	// 1. The SystemBundles are correctly expanded.
-	// 2. The Systems with implicit dependencies are correctly taken into account.
-	// 3. The Systems with explicit dependencies are correctly taken into account.
-	// 4. The Systems implicit priority, where the system registered before has
-	//		the priority is correctly resolved.
-	// 5. The Systems explicit priority, build with (`after` and `before`) is
-	//		correctly resolved.
-	// 6. All the above must be valid for C++ and Scripted systems.
-	//
-	// To verify the above we are going the build the following pipeline:
-	//
-
-	StringName system_1_name = "PipeBuilderSystem1.gd";
+	ECS::register_system(test_A_system_1, "test_A_system_1");
 
 	{
 		// Create the script.
@@ -35,26 +71,86 @@ TEST_CASE("[Modules][ECS] Test Pipeline builder.") {
 		code += "extends System\n";
 		code += "\n";
 		code += "func _prepare():\n";
-		code += "	with_component(ECS.TransformComponent, MUTABLE)\n";
+		code += "	with_component(ECS.PbComponentA, IMMUTABLE)\n";
+		code += "	with_component(ECS.PbComponentB, MUTABLE)\n";
 		code += "\n";
-		code += "func _for_each(transform_com):\n";
-		code += "	if get_current_entity_id() == 2:\n";
-		code += "		transform_com.transform.origin.x = 10.0\n";
+		code += "func _for_each(a, b):\n";
+		code += "	pass\n";
 		code += "\n";
 
-		CHECK(build_and_register_ecs_script(system_1_name, code));
+		CHECK(build_and_register_ecs_script("test_A_system_2.gd", code));
 	}
+
+	ECS::register_system(test_A_system_3, "test_A_system_3");
+
+	{
+		// Create the script.
+		String code;
+		code += "extends System\n";
+		code += "\n";
+		code += "func _prepare():\n";
+		code += "	with_component(ECS.PbComponentA, IMMUTABLE)\n";
+		code += "	with_component(ECS.PbComponentB, IMMUTABLE)\n";
+		code += "\n";
+		code += "func _for_each(a, b):\n";
+		code += "	pass\n";
+		code += "\n";
+
+		CHECK(build_and_register_ecs_script("test_A_system_4.gd", code));
+	}
+
+	ECS::register_system(test_A_system_5, "test_A_system_5");
+
+	{
+		// Create the script.
+		String code;
+		code += "extends System\n";
+		code += "\n";
+		code += "func _prepare():\n";
+		code += "	with_component(ECS.PbComponentA, IMMUTABLE)\n";
+		code += "	with_component(ECS.PbComponentB, IMMUTABLE)\n";
+		code += "	with_databag(ECS.PbDatabagA, IMMUTABLE)\n";
+		code += "\n";
+		code += "func _for_each(a, b, c):\n";
+		code += "	pass\n";
+		code += "\n";
+
+		CHECK(build_and_register_ecs_script("test_A_system_6.gd", code));
+	}
+
+	flush_ecs_script_preparation();
 
 	Pipeline pipeline;
-	{
-		Vector<StringName> system_bundles;
-		system_bundles.push_back(StringName(""));
 
-		Vector<StringName> systems;
-		systems.push_back(StringName("System1.gd"));
+	Vector<StringName> system_bundles;
 
-		PipelineBuilder::build_pipeline(system_bundles, systems, &pipeline);
-	}
+	Vector<StringName> systems;
+	// Insert the systems with random order.
+	systems.push_back(StringName("test_A_system_6.gd"));
+	systems.push_back(StringName("test_A_system_5"));
+	systems.push_back(StringName("test_A_system_4.gd"));
+	systems.push_back(StringName("test_A_system_3"));
+	systems.push_back(StringName("test_A_system_2.gd"));
+	systems.push_back(StringName("test_A_system_1"));
+
+	PipelineBuilder::build_pipeline(system_bundles, systems, &pipeline);
+
+	// Verify the test_A_system_1 is the first one being executed,
+	// has its implicit registration order imply.
+	// TODO
+
+	// Verify the test_A_system_2.gd is the second one being executed,
+	// has its implicit registration order imply, and not in parallel with
+	// test_A_system_1.
+	// TODO
+
+	// Verify the test_A_system_3 and test_A_system_4.gd run in parallel
+	// and after test_A_system_2.gd
+	// TODO
+
+	// Verify the test_A_system_5 and test_A_system_6.gd run in parallel,
+	// but after test_A_system_3 and test_A_system_4.gd.
+	// TODO
 
 	finalize_script_ecs();
 }
