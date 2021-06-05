20258fb727595996128135dd8bcda9cba78b5e9d
AndreaCatania <info@andreacatania.com>
2021-06-05 12:06:01 +0200

    Add Unit Tests to verify the SystemBundles works correctly.
    Add unit tests to verify the World and SceneTreeDatabag are always
    processed in single thread.

--- tests/test_ecs_pipeline_builder.h
+++ tests/test_ecs_pipeline_builder.h
@@ -30,7 +30,6 @@
 //    so two systems that fetch the same component mutable may run in parallel
 //    thanks to the specialization given by `Not`.
 // 7. All the above must be valid for C++ and Scripted systems.
-// 8. Include sub pipeline composition
 // 9. Make sure that the systems that fetch World, SceneTreeDatabag are always
 //     are always executed in single thread.
 // 10. Detect when an event isn't catched by any system, tell how to fix it.
@@ -65,7 +64,6 @@ void test_A_system_14(Query<const PbComponentA, const PbComponentB> &p_query) {}
 void test_A_system_15(Query<PbComponentA, PbComponentB> &p_query) {}
 
 namespace godex_tests {
-
 TEST_CASE("[Modules][ECS] Verify the PipelineBuilder takes into account implicit and explicit dependencies.") {
 	initialize_script_ecs();
 
@@ -318,11 +316,14 @@ TEST_CASE("[Modules][ECS] Verify the PipelineBuilder takes into account implicit
 	// The phase config merges with the process phase:
 
 	// test_A_system_14 has an explicit dependency with test_A_system_9, it runs
-	// before. However, the test_A_system_7.gd and test_A_system_0.gd is compatible
+	// before.
+	CHECK(stage_test_A_system_14 < stage_test_A_system_9);
+
+	// However, the test_A_system_7.gd and test_A_system_0.gd is compatible
 	// to run with with both in parallel. The optimization algorithm will decide
 	// where to put this sysetm.
-	CHECK((stage_test_A_system_7 == stage_test_A_system_14 || stage_test_A_system_7 == stage_test_A_system_9));
-	CHECK((stage_test_A_system_0 == stage_test_A_system_14 || stage_test_A_system_0 == stage_test_A_system_9));
+	CHECK(stage_test_A_system_7 < stage_test_A_system_1);
+	CHECK(stage_test_A_system_0 < stage_test_A_system_1);
 
 	// Verify the test_A_system_9.gd is executed before test_A_system_1 and in
 	// parallel with test_A_system_0.gd
@@ -332,15 +333,17 @@ TEST_CASE("[Modules][ECS] Verify the PipelineBuilder takes into account implicit
 	// an implicit dependency and test_A_system_1 is registered before.
 	CHECK(stage_test_A_system_1 < stage_test_A_system_2);
 
-	// Verify the test_A_system_3 and test_A_system_4.gd run in parallel
-	// and after test_A_system_2.gd
-	CHECK(stage_test_A_system_3 == stage_test_A_system_4);
+	// Verify the test_A_system_3 and test_A_system_4.gd may run in parallel
+	// but always after test_A_system_2.gd
+	CHECK(stage_test_A_system_4 > stage_test_A_system_2);
 	CHECK(stage_test_A_system_3 > stage_test_A_system_2);
 
-	// Verify the test_A_system_5 and test_A_system_6.gd run in parallel,
-	// but after test_A_system_3 and test_A_system_4.gd.
-	CHECK(stage_test_A_system_5 == stage_test_A_system_6);
+	// Verify the test_A_system_5 and test_A_system_6.gd may run in parallel,
+	// but always after test_A_system_3 and test_A_system_4.gd.
+	CHECK(stage_test_A_system_6 > stage_test_A_system_3);
 	CHECK(stage_test_A_system_5 > stage_test_A_system_3);
+	CHECK(stage_test_A_system_6 > stage_test_A_system_4);
+	CHECK(stage_test_A_system_5 > stage_test_A_system_4);
 
 	// Verify the test_A_system_8 run the last one, since it's marked as post
 	// process.
@@ -359,6 +362,388 @@ TEST_CASE("[Modules][ECS] Verify the PipelineBuilder takes into account implicit
 
 	// Verify the test_A_system_14 is executed before test_A_system_9.
 	CHECK(stage_test_A_system_14 < stage_test_A_system_9);
+
+	finalize_script_ecs();
+}
+} // namespace godex_tests
+
+void test_B_system_1(Query<PbComponentA> &p_query) {}
+void test_B_system_2(Query<PbComponentA> &p_query) {}
+void test_B_system_3(Query<PbComponentA> &p_query) {}
+void test_B_system_4(Query<PbComponentA> &p_query) {}
+void test_B_system_9(Query<PbComponentA> &p_query) {}
+void test_B_system_10(Query<PbComponentA> &p_query) {}
+void test_B_system_12(Query<const PbComponentA> &p_query) {}
+
+namespace godex_tests {
+TEST_CASE("[Modules][ECS] Verify the PipelineBuilder bundles.") {
+	// We have three bundles:
+	// 1. C++ only
+	// 2. GDScript only
+	// 3. GDScript that bundles C++ systems
+	// The bundle 1, depends explicitely on the system of bundle 2.
+	// Add one syngle system with explicit dependency.
+	initialize_script_ecs();
+
+	ECS::register_system(test_B_system_12, "test_B_system_12")
+			.after("test_B_system_11.gd");
+
+	ECS::register_system_bundle("CppBundle")
+			.add(ECS::register_system(test_B_system_4, "test_B_system_4") // Registered as first on purpose, to test the phase
+							.set_phase(PHASE_POST_PROCESS))
+			.add(ECS::register_system(test_B_system_2, "test_B_system_2")
+							.set_phase(PHASE_PROCESS))
+			.add(ECS::register_system(test_B_system_3, "test_B_system_3")
+							.set_phase(PHASE_PROCESS))
+			.add(ECS::register_system(test_B_system_1, "test_B_system_1") // Registered as last on purpose, to test the phase
+							.set_phase(PHASE_CONFIG));
+
+	{
+		// Create the script.
+		String code;
+		code += "extends System\n";
+		code += "\n";
+		code += "func _prepare():\n";
+		code += "	execute_in_phase(ECS.PHASE_CONFIG)\n";
+		code += "	with_component(ECS.PbComponentB, IMMUTABLE)\n";
+		code += "\n";
+		code += "func _for_each(a):\n";
+		code += "	pass\n";
+		code += "\n";
+
+		CHECK(build_and_register_ecs_script("test_B_system_5.gd", code));
+	}
+	{
+		// Create the script.
+		String code;
+		code += "extends System\n";
+		code += "\n";
+		code += "func _prepare():\n";
+		code += "	with_component(ECS.PbComponentB, IMMUTABLE)\n";
+		code += "\n";
+		code += "func _for_each(a):\n";
+		code += "	pass\n";
+		code += "\n";
+
+		CHECK(build_and_register_ecs_script("test_B_system_6.gd", code));
+	}
+	{
+		// Create the script.
+		String code;
+		code += "extends SystemBundle\n";
+		code += "\n";
+		code += "func _prepare():\n";
+		code += "	add(ECS.test_B_system_5_gd)\n";
+		code += "	add(ECS.test_B_system_6_gd)\n";
+		code += "\n";
+
+		CHECK(build_and_register_ecs_script("GDSBundle.gd", code));
+	}
+
+	// -- GDS that uses C++ --
+
+	{
+		// Create the script.
+		String code;
+		code += "extends System\n";
+		code += "\n";
+		code += "func _prepare():\n";
+		code += "	execute_in_phase(ECS.PHASE_CONFIG)\n";
+		code += "	with_component(ECS.PbComponentA, IMMUTABLE)\n";
+		code += "\n";
+		code += "func _for_each(a, b):\n";
+		code += "	pass\n";
+		code += "\n";
+
+		CHECK(build_and_register_ecs_script("test_B_system_7.gd", code));
+	}
+	{
+		// Create the script.
+		String code;
+		code += "extends System\n";
+		code += "\n";
+		code += "func _prepare():\n";
+		code += "	with_component(ECS.PbComponentA, MUTABLE)\n";
+		code += "	with_databag(ECS.PbDatabagA, MUTABLE)\n";
+		code += "\n";
+		code += "func _for_each(a, b):\n";
+		code += "	pass\n";
+		code += "\n";
+
+		CHECK(build_and_register_ecs_script("test_B_system_8.gd", code));
+	}
+
+	ECS::register_system(test_B_system_9, "test_B_system_9");
+	ECS::register_system(test_B_system_10, "test_B_system_10");
+
+	{
+		// Create the script.
+		String code;
+		code += "extends SystemBundle\n";
+		code += "\n";
+		code += "func _prepare():\n";
+		code += "	execute_before(ECS.test_B_system_3)\n";
+		code += "	execute_after(ECS.test_B_system_11_gd)\n";
+		code += "	add(ECS.test_B_system_9)\n";
+		code += "	add(ECS.test_B_system_7_gd)\n";
+		code += "	add(ECS.test_B_system_8_gd)\n";
+		code += "	add(ECS.test_B_system_10)\n";
+		code += "\n";
+
+		CHECK(build_and_register_ecs_script("GDSUsesCppBundle.gd", code));
+	}
+
+	{
+		// Create the script.
+		String code;
+		code += "extends System\n";
+		code += "\n";
+		code += "func _prepare():\n";
+		code += "	with_component(ECS.PbComponentA, IMMUTABLE)\n";
+		code += "\n";
+		code += "func _for_each(a):\n";
+		code += "	pass\n";
+		code += "\n";
+
+		CHECK(build_and_register_ecs_script("test_B_system_11.gd", code));
+	}
+
+	flush_ecs_script_preparation();
+
+	Vector<StringName> system_bundles;
+	system_bundles.push_back("CppBundle");
+	system_bundles.push_back("GDSBundle.gd");
+	system_bundles.push_back("GDSUsesCppBundle.gd");
+
+	Vector<StringName> systems;
+	systems.push_back("test_B_system_11.gd");
+	systems.push_back("test_B_system_12");
+
+	Pipeline pipeline;
+	PipelineBuilder::build_pipeline(system_bundles, systems, &pipeline);
+
+	// C++ bundle
+	const int stage_test_B_system_1 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_B_system_1")));
+	const int stage_test_B_system_2 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_B_system_2")));
+	const int stage_test_B_system_3 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_B_system_3")));
+	const int stage_test_B_system_4 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_B_system_4")));
+
+	// test_B_system_1 is marked to run in config phase, and has an implicit
+	// dependecy with test_B_system_2.
+	CHECK(stage_test_B_system_1 < stage_test_B_system_2);
+
+	// test_B_system_2 has an implicit dependecy with test_B_system_3, and its
+	// explicit priority (given by the system bundle insert order) forces it to
+	// run before.
+	CHECK(stage_test_B_system_2 < stage_test_B_system_3);
+
+	// test_B_system_3 has an implicit dependecy with test_B_system_4 that is
+	// marked to run in phase POST_PROCESS, so it runs as last.
+	CHECK(stage_test_B_system_3 < stage_test_B_system_4);
+
+	// GDScript bundle
+	const int stage_test_B_system_5 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_B_system_5.gd")));
+	const int stage_test_B_system_6 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_B_system_6.gd")));
+
+	// The GDSBundle add two systems that even if run in differn phases they have
+	// no implicit or explicit dependencies with anything else.
+	// They can run in the same thread or one after the other depending how the
+	// optimizer moves it:
+	CHECK(stage_test_B_system_5 <= stage_test_B_system_6);
+
+	// GDScript + cpp bundle
+
+	const int stage_test_B_system_7 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_B_system_7.gd")));
+	const int stage_test_B_system_8 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_B_system_8.gd")));
+	const int stage_test_B_system_9 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_B_system_9")));
+	const int stage_test_B_system_10 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_B_system_10")));
+
+	// This bundle has an explicit dependency with system 6, all systems must run
+	// before.
+	CHECK(stage_test_B_system_7 < stage_test_B_system_3);
+	CHECK(stage_test_B_system_8 < stage_test_B_system_3);
+	CHECK(stage_test_B_system_9 < stage_test_B_system_3);
+	CHECK(stage_test_B_system_10 < stage_test_B_system_3);
+
+	// Make sure the explicit priority set by the bundle is respected.
+	CHECK(stage_test_B_system_9 < stage_test_B_system_7);
+	CHECK(stage_test_B_system_7 < stage_test_B_system_8);
+	CHECK(stage_test_B_system_8 < stage_test_B_system_10);
+
+	// Verify spare systems order.
+
+	const int stage_test_B_system_11 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_B_system_11.gd")));
+	const int stage_test_B_system_12 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_B_system_12")));
+
+	CHECK(stage_test_B_system_11 < stage_test_B_system_9);
+	CHECK(stage_test_B_system_11 < stage_test_B_system_12);
+
+	finalize_script_ecs();
+}
+} // namespace godex_tests
+
+void test_C_system_1(Query<const PbComponentA> &p_query) {}
+void test_C_system_2(Query<const PbComponentA> &p_query) {}
+void test_C_system_3(Query<const PbComponentA> &p_query) {}
+void test_C_system_4(const World *) {}
+void test_C_system_5(const SceneTreeDatabag *) {}
+void test_C_system_6(Query<const PbComponentA> &p_query) {}
+void test_C_system_7(Query<const PbComponentA> &p_query) {}
+
+namespace godex_tests {
+TEST_CASE("[Modules][ECS] Verify the PipelineBuilder bundles.") {
+	initialize_script_ecs();
+
+	ECS::register_system_bundle("TestC_CppBundle")
+			.add(ECS::register_system(test_C_system_1, "test_C_system_1"))
+			.add(ECS::register_system(test_C_system_2, "test_C_system_2"))
+			.add(ECS::register_system(test_C_system_3, "test_C_system_3"))
+			.add(ECS::register_system(test_C_system_4, "test_C_system_4"))
+			.add(ECS::register_system(test_C_system_5, "test_C_system_5"))
+			.add(ECS::register_system(test_C_system_6, "test_C_system_6"))
+			.add(ECS::register_system(test_C_system_7, "test_C_system_7"));
+
+	{
+		// Create the script.
+		String code;
+		code += "extends System\n";
+		code += "\n";
+		code += "func _prepare():\n";
+		code += "	with_component(ECS.PbComponentA, IMMUTABLE)\n";
+		code += "	with_databag(ECS.World, IMMUTABLE)\n";
+		code += "\n";
+		code += "func _for_each(a, b):\n";
+		code += "	pass\n";
+		code += "\n";
+
+		CHECK(build_and_register_ecs_script("test_C_system_8.gd", code));
+	}
+	{
+		// Create the script.
+		String code;
+		code += "extends System\n";
+		code += "\n";
+		code += "func _prepare():\n";
+		code += "	with_component(ECS.PbComponentA, IMMUTABLE)\n";
+		code += "	with_databag(ECS.SceneTreeDatabag, IMMUTABLE)\n";
+		code += "\n";
+		code += "func _for_each(a, b):\n";
+		code += "	pass\n";
+		code += "\n";
+
+		CHECK(build_and_register_ecs_script("test_C_system_9.gd", code));
+	}
+	{
+		// Create the script.
+		String code;
+		code += "extends System\n";
+		code += "\n";
+		code += "func _prepare():\n";
+		code += "	with_component(ECS.PbComponentA, IMMUTABLE)\n";
+		code += "\n";
+		code += "func _for_each(a):\n";
+		code += "	pass\n";
+		code += "\n";
+
+		CHECK(build_and_register_ecs_script("test_C_system_10.gd", code));
+	}
+	{
+		// Create the script.
+		String code;
+		code += "extends System\n";
+		code += "\n";
+		code += "func _prepare():\n";
+		code += "	with_component(ECS.PbComponentA, IMMUTABLE)\n";
+		code += "\n";
+		code += "func _for_each(a):\n";
+		code += "	pass\n";
+		code += "\n";
+
+		CHECK(build_and_register_ecs_script("test_C_system_11.gd", code));
+	}
+	{
+		// Create the script.
+		String code;
+		code += "extends SystemBundle\n";
+		code += "\n";
+		code += "func _prepare():\n";
+		code += "	add(ECS.test_C_system_8_gd)\n";
+		code += "	add(ECS.test_C_system_9_gd)\n";
+		code += "	add(ECS.test_C_system_10_gd)\n";
+		code += "	add(ECS.test_C_system_11_gd)\n";
+		code += "\n";
+
+		CHECK(build_and_register_ecs_script("TestC_GDSBundle.gd", code));
+	}
+
+	flush_ecs_script_preparation();
+
+	Vector<StringName> system_bundles;
+	system_bundles.push_back("TestC_CppBundle");
+	system_bundles.push_back("TestC_GDSBundle.gd");
+
+	Vector<StringName> systems;
+
+	Pipeline pipeline;
+	PipelineBuilder::build_pipeline(system_bundles, systems, &pipeline);
+
+	const int stage_test_C_system_1 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_C_system_1")));
+	const int stage_test_C_system_2 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_C_system_2")));
+	const int stage_test_C_system_3 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_C_system_3")));
+	const int stage_test_C_system_4 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_C_system_4")));
+	const int stage_test_C_system_5 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_C_system_5")));
+	const int stage_test_C_system_6 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_C_system_6")));
+	const int stage_test_C_system_7 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_C_system_7")));
+	const int stage_test_C_system_8 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_C_system_8.gd")));
+	const int stage_test_C_system_9 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_C_system_9.gd")));
+	const int stage_test_C_system_10 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_C_system_10.gd")));
+	const int stage_test_C_system_11 = pipeline.get_system_stage(ECS::get_system_id(StringName("test_C_system_11.gd")));
+
+	// Make sure the systems that fetch World an SceneTreeDatabag runs always in
+	// single thread.
+	CHECK(stage_test_C_system_4 != stage_test_C_system_1);
+	CHECK(stage_test_C_system_4 != stage_test_C_system_2);
+	CHECK(stage_test_C_system_4 != stage_test_C_system_3);
+	CHECK(stage_test_C_system_4 != stage_test_C_system_5);
+	CHECK(stage_test_C_system_4 != stage_test_C_system_6);
+	CHECK(stage_test_C_system_4 != stage_test_C_system_7);
+	CHECK(stage_test_C_system_4 != stage_test_C_system_8);
+	CHECK(stage_test_C_system_4 != stage_test_C_system_9);
+	CHECK(stage_test_C_system_4 != stage_test_C_system_10);
+	CHECK(stage_test_C_system_4 != stage_test_C_system_11);
+
+	CHECK(stage_test_C_system_5 != stage_test_C_system_1);
+	CHECK(stage_test_C_system_5 != stage_test_C_system_2);
+	CHECK(stage_test_C_system_5 != stage_test_C_system_3);
+	CHECK(stage_test_C_system_5 != stage_test_C_system_4);
+	CHECK(stage_test_C_system_5 != stage_test_C_system_6);
+	CHECK(stage_test_C_system_5 != stage_test_C_system_7);
+	CHECK(stage_test_C_system_5 != stage_test_C_system_8);
+	CHECK(stage_test_C_system_5 != stage_test_C_system_9);
+	CHECK(stage_test_C_system_5 != stage_test_C_system_10);
+	CHECK(stage_test_C_system_5 != stage_test_C_system_11);
+
+	CHECK(stage_test_C_system_8 != stage_test_C_system_1);
+	CHECK(stage_test_C_system_8 != stage_test_C_system_2);
+	CHECK(stage_test_C_system_8 != stage_test_C_system_3);
+	CHECK(stage_test_C_system_8 != stage_test_C_system_4);
+	CHECK(stage_test_C_system_8 != stage_test_C_system_5);
+	CHECK(stage_test_C_system_8 != stage_test_C_system_6);
+	CHECK(stage_test_C_system_8 != stage_test_C_system_7);
+	CHECK(stage_test_C_system_8 != stage_test_C_system_9);
+	CHECK(stage_test_C_system_8 != stage_test_C_system_10);
+	CHECK(stage_test_C_system_8 != stage_test_C_system_11);
+
+	CHECK(stage_test_C_system_9 != stage_test_C_system_1);
+	CHECK(stage_test_C_system_9 != stage_test_C_system_2);
+	CHECK(stage_test_C_system_9 != stage_test_C_system_3);
+	CHECK(stage_test_C_system_9 != stage_test_C_system_4);
+	CHECK(stage_test_C_system_9 != stage_test_C_system_5);
+	CHECK(stage_test_C_system_9 != stage_test_C_system_6);
+	CHECK(stage_test_C_system_9 != stage_test_C_system_7);
+	CHECK(stage_test_C_system_9 != stage_test_C_system_8);
+	CHECK(stage_test_C_system_9 != stage_test_C_system_10);
+	CHECK(stage_test_C_system_9 != stage_test_C_system_11);
 
 	finalize_script_ecs();
 }
