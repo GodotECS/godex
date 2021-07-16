1ed9ed20bf8eb3caa2d2e55eb23c851d77cc1e37
AndreaCatania <info@andreacatania.com>
2021-07-16 09:32:21 +0200

    Update godot 60add98a4cff0d27c6aa2f52042555eccb86ed70

--- modules/godot/editor_plugins/editor_world_ecs.cpp
+++ modules/godot/editor_plugins/editor_world_ecs.cpp
@@ -1264,7 +1264,7 @@ void EditorWorldECS::add_error(const String &p_msg) {
 	Label *lbl = memnew(Label);
 	lbl->set_text("- [Error] " + p_msg);
 	lbl->add_theme_color_override("font_color", Color(0.95, 0.05, 0));
-	lbl->set_autowrap(true);
+	lbl->set_autowrap_mode(Label::AutowrapMode::AUTOWRAP_WORD_SMART);
 	errors_warnings_container->add_child(lbl);
 }
 
@@ -1272,7 +1272,7 @@ void EditorWorldECS::add_warning(const String &p_msg) {
 	Label *lbl = memnew(Label);
 	lbl->set_text("- [Warning] " + p_msg);
 	lbl->add_theme_color_override("font_color", Color(0.96, 0.9, 0.45));
-	lbl->set_autowrap(true);
+	lbl->set_autowrap_mode(Label::AutowrapMode::AUTOWRAP_WORD_SMART);
 	errors_warnings_container->add_child(lbl);
 }
 
