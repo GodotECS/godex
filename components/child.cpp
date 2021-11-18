2eb962213ec0cd88326fbbd854d8475ef4918744
Lewis James <lewiji+github@gmail.com>
2021-11-18 11:33:56 +0000

    Change PROPERTY_USAGE_NOEDITOR to PROPERTY_USAGE_NO_EDITOR

--- components/child.cpp
+++ components/child.cpp
@@ -1,7 +1,7 @@
 #include "child.h"
 
 void Child::_bind_methods() {
-	ECS_BIND_PROPERTY(Child, PropertyInfo(Variant::INT, "parent", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NOEDITOR), parent);
+	ECS_BIND_PROPERTY(Child, PropertyInfo(Variant::INT, "parent", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NO_EDITOR), parent);
 }
 
 void Child::_get_storage_config(Dictionary &r_dictionary) {
